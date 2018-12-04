36796339a4be64c267fb652571dfb7c1d5b5901b
Eoin Kelly <Ekelly@Eoins-MacBook-Pro.local>
2018-12-04 14:51:11 +0000

    Changed tag

--- app/src/main/java/org/wit/autochangelog/MainActivity.java
+++ app/src/main/java/org/wit/autochangelog/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     public String helloWorld;
     public boolean yes;
-    private long thisis;
+
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
