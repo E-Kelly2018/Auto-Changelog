1713cee76bf72779e6ffd327b2786e975f057981
Eoin Kelly <Ekelly@Eoins-MacBook-Pro.local>
2018-12-04 13:16:43 +0000

    added int

--- app/src/main/java/org/wit/autochangelog/MainActivity.java
+++ app/src/main/java/org/wit/autochangelog/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public String helloWorld;
+    public int num;
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
